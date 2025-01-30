2e3c2244e3e2cf5a52e367a6b78f3f67ea186137
saramoh2309 <86772218+saramoh2309@users.noreply.github.com>
2025-01-30 12:14:52 +0100

    added logging to chat view cmp

--- force-app/main/default/lwc/nksChatView/nksChatView.js
+++ force-app/main/default/lwc/nksChatView/nksChatView.js
@@ -1,27 +1,26 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import getThreadId from '@salesforce/apex/nksChatView.getThreadId';
 import markasread from '@salesforce/apex/CRM_MessageHelperExperience.markAsRead';
 import getChatbotMessage from '@salesforce/apex/nksChatView.getChatbotMessage';
 import { publish, MessageContext } from 'lightning/messageService';
 import globalModalOpen from '@salesforce/messageChannel/globalModalOpen__c';
 import userId from '@salesforce/user/Id';
-
-///////////// Extra import
 import getmessages from '@salesforce/apex/CRM_MessageHelperExperience.getMessagesFromThread';
 import getContactId from '@salesforce/apex/CRM_MessageHelperExperience.getUserContactId';
+import {logModalEvent} from 'c/amplitude';
 
 export default class NksChatView extends LightningElement {
     @api recordId;
+
     threadId;
     errorList = { title: '', errors: [] };
     modalOpen = false;
-    @track chatbotMessage = 'Laster inn samtale';
+    chatbotMessage = 'Laster inn samtale';
+    userContactId;
+    messages;
 
     @wire(MessageContext)
     messageContext;
-
-    userContactId;
-    messages;
 
     connectedCallback() {
         getContactId({})
@@ -44,6 +43,15 @@ export default class NksChatView extends LightningElement {
         }
     }
 
+    @wire(getmessages, { threadId: '$threadId' }) //Calls apex and extracts messages related to this record
+    wiremessages(result) {
+        if (result.error) {
+            this.error = result.error;
+        } else if (result.data) {
+            this.messages = result.data;
+        }
+    }
+
     handleValidation() {
         this.errorList = {
             title: '',
@@ -63,6 +71,8 @@ export default class NksChatView extends LightningElement {
         getChatbotMessage({ chatId: this.recordId, userId: userId }).then((res) => {
             this.chatbotMessage = res;
         });
+
+        logModalEvent(true, 'Chatbot samtale', 'nksChatView', 'Chatsamtale' );
     }
 
     closeModal() {
@@ -70,6 +80,8 @@ export default class NksChatView extends LightningElement {
         publish(this.messageContext, globalModalOpen, { status: 'false' });
         const btn = this.template.querySelector('.focusBtn');
         btn.focus();
+
+        logModalEvent(false, 'Chatbot samtale', 'nksChatView', 'Chatsamtale' );
     }
 
     handleKeyboardEvent(event) {
@@ -82,15 +94,5 @@ export default class NksChatView extends LightningElement {
 
     get termsModal() {
         return this.template.querySelector('c-community-modal');
-    }
-    /////////////////////////////////////////////////////////////
-
-    @wire(getmessages, { threadId: '$threadId' }) //Calls apex and extracts messages related to this record
-    wiremessages(result) {
-        if (result.error) {
-            this.error = result.error;
-        } else if (result.data) {
-            this.messages = result.data;
-        }
     }
 }
